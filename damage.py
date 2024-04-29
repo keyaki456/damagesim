ad98919190774164b373e71b913663ce02b49037
tamiyokeyaki <122135530+keyaki456@users.noreply.github.com>
2024-04-29 17:59:56 +0000

    aいあうえおり

--- damage.py
+++ damage.py
@@ -1,7 +1,55 @@
 import streamlit as st
-import pandas as pd
+st.title('サマナクロ宝石ルーン銀河石魔法書シミュ')
+st.write('召喚士の武器を外して、召喚獣のルーンを外した状態の、召喚獣の攻撃力とクリダメとクリ率をここに手入力する')
+coll1, coll2, coll3 = st.columns(3)
+with coll1:
+    Attack = st.number_input('攻撃力', 0)
+with coll2:
+    CliDam = st.number_input('クリダメ', 0.0)
+with coll3:
+    Cliper = st.number_input('クリ率', 0.0)
  
-st.write("""
-# My first app
-Hello *world!*
-""")
+container = st.container(border=True)
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        selected_item = st.selectbox('火武器の赤宝石',
+                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
+                        '攻撃%S', '攻撃%A','攻撃%B',
+                        'クリダメS', 'クリダメA','クリダメB',
+                        'クリ率S', 'クリ率A','クリ率B'
+                        ])
+        selected_item = st.selectbox('水武器の赤宝石',
+                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
+                        '攻撃%S', '攻撃%A','攻撃%B',
+                        'クリダメS', 'クリダメA','クリダメB',
+                        'クリ率S', 'クリ率A','クリ率B'
+                        ])
+        selected_item = st.selectbox('風武器の赤宝石',
+                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
+                        '攻撃%S', '攻撃%A','攻撃%B',
+                        'クリダメS', 'クリダメA','クリダメB',
+                        'クリ率S', 'クリ率A','クリ率B'
+                        ])
+    with col2:
+        selected_item = st.selectbox('光武器の赤宝石',
+                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
+                        '攻撃%S', '攻撃%A','攻撃%B',
+                        'クリダメS', 'クリダメA','クリダメB',
+                        'クリ率S', 'クリ率A','クリ率B'
+                        ])
+        selected_item = st.selectbox('闇武器の赤宝石',
+                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
+                        '攻撃%S', '攻撃%A','攻撃%B',
+                        'クリダメS', 'クリダメA','クリダメB',
+                        'クリ率S', 'クリ率A','クリ率B'
+                        ])
+        selected_item = st.selectbox(
+                        'サポート武器の赤宝石',
+                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
+                        '攻撃%S', '攻撃%A','攻撃%B',
+                        'クリダメS', 'クリダメA','クリダメB',
+                        'クリ率S', 'クリ率A','クリ率B'
+                        ])
+
+

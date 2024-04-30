19a14fcd791b06b9b01ac7d3ddcb26a62cd8fb29
tamiyokeyaki <122135530+keyaki456@users.noreply.github.com>
2024-04-30 12:05:55 +0000

    超過

--- damage.py
+++ damage.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.title('サマナクロ宝石ルーン銀河石魔法書シミュ')
 st.write('召喚士の武器を外して、召喚獣のルーンを外した状態の、召喚獣の攻撃力とクリダメとクリ率をここに手入力する')
+
 coll1, coll2, coll3 = st.columns(3)
 with coll1:
     Attack = st.number_input('攻撃力', 0)
@@ -14,55 +15,34 @@ container = st.container(border=True)
 with st.container():
     col1, col2,col3,col4 = st.columns(4)
     with col1:
-        selected_item = st.selectbox('火武器の赤宝石',
-                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
-                        '攻撃%S', '攻撃%A','攻撃%B',
-                        'クリダメS', 'クリダメA','クリダメB',
-                        'クリ率S', 'クリ率A','クリ率B'
-                        ])
-        selected_item = st.selectbox('水武器の赤宝石',
-                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
-                        '攻撃%S', '攻撃%A','攻撃%B',
-                        'クリダメS', 'クリダメA','クリダメB',
-                        'クリ率S', 'クリ率A','クリ率B'
-                        ])
-        selected_item = st.selectbox('風武器の赤宝石',
-                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
-                        '攻撃%S', '攻撃%A','攻撃%B',
-                        'クリダメS', 'クリダメA','クリダメB',
-                        'クリ率S', 'クリ率A','クリ率B'
-                        ])
+        selected_item = st.selectbox('火武器の赤宝石',['攻撃実数S', '攻撃実数A','攻撃実数B','攻撃%S', '攻撃%A','攻撃%B','クリダメS', 'クリダメA','クリダメB','クリ率S', 'クリ率A','クリ率B'])
+        selected_item = st.selectbox('水武器の赤宝石',['攻撃実数S', '攻撃実数A','攻撃実数B','攻撃%S', '攻撃%A','攻撃%B','クリダメS', 'クリダメA','クリダメB','クリ率S', 'クリ率A','クリ率B'])
+        selected_item = st.selectbox('風武器の赤宝石',['攻撃実数S', '攻撃実数A','攻撃実数B','攻撃%S', '攻撃%A','攻撃%B','クリダメS', 'クリダメA','クリダメB','クリ率S', 'クリ率A','クリ率B'])
     with col2:
-        selected_item = st.selectbox('光武器の赤宝石',
-                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
-                        '攻撃%S', '攻撃%A','攻撃%B',
-                        'クリダメS', 'クリダメA','クリダメB',
-                        'クリ率S', 'クリ率A','クリ率B'
-                        ])
-        selected_item = st.selectbox('闇武器の赤宝石',
-                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
-                        '攻撃%S', '攻撃%A','攻撃%B',
-                        'クリダメS', 'クリダメA','クリダメB',
-                        'クリ率S', 'クリ率A','クリ率B'
-                        ])
-        selected_item = st.selectbox('サポート武器赤宝石',
-                        ['攻撃実数S', '攻撃実数A','攻撃実数B',
-                        '攻撃%S', '攻撃%A','攻撃%B',
-                        'クリダメS', 'クリダメA','クリダメB',
-                        'クリ率S', 'クリ率A','クリ率B'])
+        selected_item = st.selectbox('光武器の赤宝石',['攻撃実数S', '攻撃実数A','攻撃実数B','攻撃%S', '攻撃%A','攻撃%B','クリダメS', 'クリダメA','クリダメB','クリ率S', 'クリ率A','クリ率B'])
+        selected_item = st.selectbox('闇武器の赤宝石',['攻撃実数S', '攻撃実数A','攻撃実数B','攻撃%S', '攻撃%A','攻撃%B','クリダメS', 'クリダメA','クリダメB','クリ率S', 'クリ率A','クリ率B'])
+        selected_item = st.selectbox('サポート武器赤宝石',['攻撃実数S', '攻撃実数A','攻撃実数B','攻撃%S', '攻撃%A','攻撃%B','クリダメS', 'クリダメA','クリダメB','クリ率S', 'クリ率A','クリ率B'])
     with col3:
-                selected_item = st.selectbox('ルーン1主オプ',
-                        ['攻撃実数'])
-                selected_item = st.selectbox('ルーン2主オプ',
-                        ['その他','攻撃実数','攻撃%','クリダメ','クリ率'])
-                selected_item = st.selectbox('ルーン3主オプ',
-                        ['防御実数'])
+        selected_item = st.selectbox('ルーン1主オプ',['攻撃実数'])
+        selected_item = st.selectbox('ルーン2主オプ',['その他','攻撃実数','攻撃%','クリダメ','クリ率'])
+        selected_item = st.selectbox('ルーン3主オプ',['防御実数'])
     with col4:
-                selected_item = st.selectbox('ルーン4主オプ',
-                        ['その他','攻撃実数','攻撃%','クリダメ','クリ率'])
-                selected_item = st.selectbox('ルーン5主オプ',
-                        ['体力実数'])
-                selected_item = st.selectbox('ルーン6主オプ',
-                        ['その他','攻撃実数','攻撃%','クリダメ','クリ率'])
+        selected_item = st.selectbox('ルーン4主オプ',['その他','攻撃実数','攻撃%','クリダメ','クリ率'])
+        selected_item = st.selectbox('ルーン5主オプ',['体力実数'])
+        selected_item = st.selectbox('ルーン6主オプ',['その他','攻撃実数','攻撃%','クリダメ','クリ率'])
 
-colll1, colll2, colll3,colll4, colll5, colll6, = st.columns(6)
+container = st.container(border=True)
+with st.container():
+        colll1, colll2, colll3,colll4, colll5, colll6, = st.columns(6)
+        with colll1:
+                st.write('ルーン1')
+        with colll2:
+                st.write('ルーン2')
+        with colll3:
+                st.write('ルーン3')
+        with colll4:
+                st.write('ルーン4')
+        with colll5:
+                st.write('ルーン5')
+        with colll6:
+                st.write('ルーン6')
